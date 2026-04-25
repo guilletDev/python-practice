fc57ac79057e6ab51d04056699f7650abf6cf570
guilletdev <guilletdev@gmail.com>
2026-04-25 18:37:15 -0300

    initial commit: variables and data types exercises

---
+++ tipo-de-datos.py
@@ -0,0 +1,11 @@
+nombre = 'Juan'  #str tipo string
+edad = 25        #int tipo integer
+altura = 1.75    #float tipo decimal
+activo = True    #bool es booleano true o false   
+
+
+print (nombre)
+print (edad) 
+print (altura)
+print (activo) 
+
---
+++ variables.py
@@ -0,0 +1,5 @@
+nombre = 'Juan'
+edad = 25
+
+print (nombre)
+print (edad) 
